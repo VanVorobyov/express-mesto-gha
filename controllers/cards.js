01a48b4549e2acd780e85c9cc3e14b0f332970c8
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-07-01 13:42:32 +0300

    feat: add handle errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,10 @@
 const Card = require('../models/card');
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../utils/errors/errors');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.status(200).send(cards))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(ERROR_DEFAULT).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
@@ -11,13 +12,30 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner })
     .then((card) => res.status(201).send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(ERROR_BAD_REQUEST)
+          .send({ message: 'Переданы некорректные данные при создании карточки' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
+    .orFail(() => new Error('Not Found'))
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.message === 'Not Found') {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: 'Карточка с указанным _id не найдена.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -26,8 +44,21 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
+    .orFail(() => new Error('Not Found'))
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.message === 'Not Found') {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: 'Передан несуществующий _id карточки.' });
+      } else if (err.name === 'CastError') {
+        res
+          .status(ERROR_BAD_REQUEST)
+          .send({ message: 'Переданы некорректные данные для постановки лайка.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
 
 module.exports.deleteLike = (req, res) => {
@@ -36,6 +67,19 @@ module.exports.deleteLike = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
+    .orFail(() => new Error('Not Found'))
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.message === 'Not Found') {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: 'Передан несуществующий _id карточки.' });
+      } else if (err.name === 'CastError') {
+        res
+          .status(ERROR_BAD_REQUEST)
+          .send({ message: 'Переданы некорректные данные для снятия лайка.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
