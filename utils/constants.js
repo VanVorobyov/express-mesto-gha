a27556e117de9e631dbe641f614c7b4572f96244
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-07-23 23:01:56 +0300

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,1 @@
-module.exports.isURL = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+module.exports.isURL = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
